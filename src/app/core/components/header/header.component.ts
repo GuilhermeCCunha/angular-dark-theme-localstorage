5b9e2d77b486155c036a7800dc594ae13c711d96
Guilherme Cunha <guilhermecdacunha@gmail.com>
2023-10-19 17:58:11 -0300

    sincronização de temas personalizados entre abas/janelas usando RxJS

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, delay, fromEvent } from 'rxjs';
 
 // Enums
 import { ETheme } from '../../enums/ETheme.enum';
@@ -11,10 +12,13 @@ import { ETheme } from '../../enums/ETheme.enum';
 export class HeaderComponent implements OnInit {
   public icon: string = ETheme.ICON_MOON;
   public textTheme: string = ETheme.TEXT_MOON;
-  public theme: boolean = false;
+  public theme: boolean | void = false;
+  public activeTheme$!: Observable<Event>;
+
   constructor() {}
 
   ngOnInit() {
+    this.init();
     try {
       this.theme = JSON.parse(localStorage.getItem('isDarkTheme') || '[]')
     }
@@ -22,9 +26,7 @@ export class HeaderComponent implements OnInit {
       this.theme
     }
     if (this.theme === true) {
-      this.theme = document.body.classList.toggle('dark-theme');
-      this.textTheme = ETheme.TEXT_SUN;
-      this.icon = this.icon = ETheme.ICON_SUN
+      this.addTheme();
     }
   }
 
@@ -38,5 +40,40 @@ export class HeaderComponent implements OnInit {
 
     this.textTheme = ETheme.TEXT_MOON;
     return (this.icon = ETheme.ICON_MOON);
+  }
+
+  public addTheme() {
+    this.theme = document.body.classList.add('dark-theme');
+    this.textTheme = ETheme.TEXT_SUN;
+    return (this.icon = ETheme.ICON_SUN);
+
+  }
+
+  public removeTheme() {
+    this.theme = document.body.classList.remove('dark-theme');
+    this.textTheme = ETheme.TEXT_MOON;
+    return (this.icon = ETheme.ICON_MOON);
+  }
+
+  init(){
+    this.activeTheme$ = fromEvent(window, 'storage');
+    this.activeTheme$.pipe(delay(50)).subscribe(
+      () => {
+        let action = localStorage.getItem('isDarkTheme');
+        if(action === 'true'){
+          this.addTheme();
+        }
+        else if (action === 'false') {
+          this.removeTheme();
+        }
+        else if(action != 'true' && action != 'false' && action != null){
+          this.removeTheme();
+          localStorage.setItem('isDarkTheme', JSON.stringify(false));
+        }
+        else {
+          this.removeTheme();
+        }
+      }
+    );
   }
 }
